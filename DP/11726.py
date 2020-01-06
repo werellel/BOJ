5774773f3aa66cb5e5161a4b4e979ccd92b51565
werellel <werellel@gmail.com>
2020-01-07 08:35:29 +0900

    '11726'

---
+++ DP/11726.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+answer = 0
+n_1 = 1
+n_2 = 2
+
+for i in range(1, n+1):
+	if i == 1:
+		answer += 1
+	elif i == 2:
+		answer += 1
+	else:
+		answer = n_1 + n_2
+		n_1 = n_2
+		n_2 = answer
+
+print(answer)
